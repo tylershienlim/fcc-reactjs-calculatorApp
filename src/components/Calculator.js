854ed8555a4bac9541c537b07a4d6f1964670870
tylershienlim <tylershienlim@gmail.com>
2023-06-01 15:20:45 +1000

    frames
    frames

---
+++ src/components/Calculator.js
@@ -0,0 +1,13 @@
+import './Calculator.css'
+import React from 'react'
+
+function Calculator() {
+  return (
+    <div className='calculatorFrame'>
+        <div className='calculatorDisplay'></div>
+        <div className='calculatorButtons'></div>
+    </div>
+  )
+}
+
+export default Calculator;
